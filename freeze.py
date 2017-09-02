20e8f46bed9d0922bed9000c0d9941608dddde71
Mark Mollineaux <bufordsharkley@gmail.com>
2017-09-01 21:11:54 -0700

    Earful of Cocktail (#12)
    * Most of the changes for the cocktail website
    
    * More fun changes
    
    * responding to pull request

--- freeze.py
+++ freeze.py
@@ -1,12 +1,28 @@
+import optparse
+
 from flask_frozen import Freezer
 
 import app
 
-freezer = Freezer(app.app)
+parser = optparse.OptionParser(usage="usage: %prog [options] podcast")
+(options, args) = parser.parse_args()
+
+if len(args) != 1:
+    parser.error("Must specify podcast")
+
+podcast = args[0].strip().lower()
+
+if podcast == 'cocktail':
+    freezer = Freezer(app.cocktail_app)
+    info = app.cocktail_info
+else:
+    freezer = Freezer(app.app)
+    info = app.convoy_info
+
 
 @freezer.register_generator
 def episode():
-    for ep in app.get_eps():
+    for ep in app.get_eps(info):
         yield {'num': ep}
 
 freezer.freeze()
