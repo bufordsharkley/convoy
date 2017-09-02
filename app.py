abe4bbc5c645becbc056a2918feb958c7d75cde2
Mark Mollineaux <bufordsharkley@gmail.com>
2017-09-01 20:21:25 -0700

    More fun changes

--- app.py
+++ app.py
@@ -102,5 +102,5 @@ def page_not_found(e):
 
 
 if __name__ == '__main__':
-    cocktail_app.run(debug=True)
-    #app.run(debug=True)
+    app.run(debug=True)
+    cocktail_app.run(debug=True, port=5001)
